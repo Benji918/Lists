a82428075958c770377139fddadaf564ed014e25
Benjamin <kodiugos@gmail.com>
2024-09-16 20:26:32 -0500

    Add project files.

---
+++ Lists/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = [2333, 24, 33355];
+
+            integers.Sort();
+
+            foreach (int i in integers)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadKey();    
+        }
+    }
+}
